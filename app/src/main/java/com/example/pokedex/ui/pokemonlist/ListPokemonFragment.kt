15390e5807af1f0747661a80cfaede5b793cee0e
apetitpas2022 <cailia@hotmail.fr>
2024-02-09 12:24:42 +0100

    Début search view

--- app/src/main/java/com/example/pokedex/ui/pokemonlist/ListPokemonFragment.kt
+++ app/src/main/java/com/example/pokedex/ui/pokemonlist/ListPokemonFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
@@ -13,8 +14,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.enishop.R
 import com.example.enishop.databinding.FragmentListPokemonBinding
 import com.example.pokedex.PokemonAdapter
+import com.example.pokedex.bo.Pokemon
 
-private const val TAG = "ListPokemonFragment"
 class ListPokemonFragment: Fragment() {
     lateinit var binding: FragmentListPokemonBinding
     val vm: ListPokemonViewModel by viewModels{ ListPokemonViewModel.Factory }
@@ -33,14 +34,32 @@ class ListPokemonFragment: Fragment() {
 
         vm.getPokemonList()
 
+        val pokemonList = ArrayList<Pokemon>()
+
         val recycler = binding.rvPokemons
 
         vm.pokemons.observe(viewLifecycleOwner) { pokemons ->
             recycler.adapter = PokemonAdapter(pokemons) {pokemon ->
+                pokemonList.add(pokemon)
+
                 val direction = ListPokemonFragmentDirections.actionListToDetail(pokemon)
                 Navigation.findNavController(view).navigate(direction)
             }
             recycler.layoutManager = LinearLayoutManager(view.context)
         }
+
+        var searchPokemon = view.findViewById<SearchView>(R.id.search_pokemon)
+
+        searchPokemon.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                PokemonAdapter(pokemonList){}.getFilter().filter(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                PokemonAdapter(pokemonList){}.getFilter().filter(newText)
+                return true
+            }
+        })
     }
 }
